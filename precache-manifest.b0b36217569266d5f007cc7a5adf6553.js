2f0e818b39a833a537be7027832bdc64282a61e0
Thanh <thanh@Thanhs-MacBook-Pro.local>
2020-08-25 23:17:47 +0700

    Updates

---
+++ precache-manifest.b0b36217569266d5f007cc7a5adf6553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91efcf2ea24cde8304e5aa82bc82224f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "797fb87a6f22f10b60da",
+    "url": "/monsters-rolodex/static/css/main.4f6fd437.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797fb87a6f22f10b60da",
+    "url": "/monsters-rolodex/static/js/main.bfd22d3c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
